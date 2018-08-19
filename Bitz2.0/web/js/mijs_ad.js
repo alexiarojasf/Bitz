21459378f84ee3cffb15ed342af5ce1c2ce29ba8
AndruGmR <35642712+AndruGmR@users.noreply.github.com>
2018-08-19 17:46:04 -0600

    Add files via upload

---
+++ Bitz2.0/web/js/mijs_ad.js
@@ -0,0 +1,20 @@
+
+    $(document).ready(function(){
+        $('.fixed-action-btn').openFAB();
+        $('.fixed-action-btn').closeFAB();
+        $('.fixed-action-btn.toolbar').openToolbar();
+        $('.fixed-action-btn.toolbar').closeToolbar();
+      });
+      $(document).ready(function(){
+        $('.modal').modal();
+        $('#modal1').modal('open');
+        $('#modal1').modal('close');
+      });   
+      $(document).ready(function() {
+        $('select').material_select();
+      });
+      $(document).ready(function(){
+        $('.modal').modal();
+        $('#modal2').modal('open');
+        $('#modal2').modal('close');
+      });
