2c38df2029b1d57a3255b87fa0275faa5803dea2
expobecas <37518924+expobecas@users.noreply.github.com>
2018-08-20 16:06:39 -0600

    IMAGES
    Faltaban ciertas imagenes

---
+++ Bitz2.0/public/recuperar.php
@@ -0,0 +1,10 @@
+<!--Index//-->
+<?php
+require_once "../app/controllers/public/controller_index.php";
+$mvc = new mvcController();
+$mvc -> plantilla();
+Page::templateHeader("Cambiar contraseña");
+require_once "../app/views/public/account/recuperar_view.php";
+Page::templateFooter();
+?>
+<!--//Index-->
